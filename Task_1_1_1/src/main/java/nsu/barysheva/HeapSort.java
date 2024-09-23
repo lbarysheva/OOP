c2b23d7ad486917f8fc1a6c608f9ffa57f5e0944
lbarysheva <lesia@MacBook-Air-Olesya.local>
2024-09-23 11:35:02 +0700

    rename "Main.java" to "HeapSort.java" and add functions

---
+++ Task_1_1_1/src/main/java/nsu/barysheva/HeapSort.java
@@ -0,0 +1,37 @@
+package nsu.barysheva;
+
+public class HeapSort {
+
+    void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+    void heapify(int[] arr, int n, int i) {
+        int largest = i, l = 2 * i + 1, r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+    }
+
+    void sort(int[] arr, int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
