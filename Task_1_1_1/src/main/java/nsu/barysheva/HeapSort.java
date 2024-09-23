c1ffcdf1072e1e304415cc0a5a15ac677398bfd9
lbarysheva <lesia@MacBook-Air-Olesya.local>
2024-09-23 14:47:41 +0700

    add some tests

--- Task_1_1_1/src/main/java/nsu/barysheva/HeapSort.java
+++ Task_1_1_1/src/main/java/nsu/barysheva/HeapSort.java
@@ -28,7 +28,9 @@ public class HeapSort {
      */
 
     void heapify(int[] arr, int n, int i) {
-        int largest = i, l = 2 * i + 1, r = 2 * i + 2;
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
 
         if (l < n && arr[l] > arr[largest]) {
             largest = l;
--- Task_1_1_1/src/test/java/nsu/barysheva/HeapSortTest.java
+++ Task_1_1_1/src/test/java/nsu/barysheva/HeapSortTest.java
@@ -7,24 +7,32 @@ class HeapSortTest {
     @Test
     void test_swap() {
         int[] arr = {1, 2, 3};
-        int[] must = {3, 2, 1};
+        int[] must_be = {3, 2, 1};
         HeapSort ob = new HeapSort();
         ob.swap(arr, 2, 0);
-        Assertions.assertArrayEquals(must, arr);
+        Assertions.assertArrayEquals(must_be, arr);
     }
 
     @Test
     void test_heapify() {
         int[] arr1 = {1, 2, 3};
-        int[] must1 = {3, 2, 1};
+        int[] must_be1 = {3, 2, 1};
         HeapSort ob = new HeapSort();
         ob.heapify(arr1, 3, 0);
-        Assertions.assertArrayEquals(must1, arr1);
+        Assertions.assertArrayEquals(must_be1, arr1);
         int[] arr2 = {1, 3, 2, 4};
-        int[] must2 = {3, 4, 2, 1};
+        int[] must_be2 = {3, 4, 2, 1};
         ob.heapify(arr2, 4, 0);
-        Assertions.assertArrayEquals(must2, arr2);
+        Assertions.assertArrayEquals(must_be2, arr2);
     }
 
+    @Test
+    void test_sort() {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] testArray = {2, 3, 1, 4, 5};
+        HeapSort ob = new HeapSort();
+        ob.sort(testArray, 5);
+        Assertions.assertArrayEquals(a, testArray);
+    }
 
 }
