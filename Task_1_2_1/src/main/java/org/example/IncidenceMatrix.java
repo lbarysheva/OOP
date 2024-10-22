4c8018ab00f342f6a8de2538b2b77f1639083401
Olesia Barysheva <o.barysheva@g.nsu.ru>
2024-10-22 23:49:31 +0700

    task 1_2_1 changes

---
+++ Task_1_2_1/src/main/java/org/example/IncidenceMatrix.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IncidenceMatrix implements Graph {
+
+    @Override
+    public void addVertex() {
+
+    }
+
+    @Override
+    public void removeVertex(int v) {
+
+    }
+
+    @Override
+    public void addEdge(Edge e) {
+
+    }
+
+    @Override
+    public int removeEdge(Edge e) {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Integer> neighbors(int v) {
+        return null;
+    }
+
+    @Override
+    public void read(InputStream input, String regex) {
+
+    }
+
+    @Override
+    public List<Integer> topologicalSort() {
+        return List.of();
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void isEqual() {
+
+    }
+}
