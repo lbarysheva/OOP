1b4a90852a8d38c1380b456ed40936ee70bb582b
Olesia Barysheva <o.barysheva@g.nsu.ru>
2024-11-19 22:05:21 +0700

    some fixes

---
+++ Task_1_3_1/build/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"nsu.barysheva","c":"SubstringSearch","l":"find(String)","u":"find(java.lang.String)"},{"p":"nsu.barysheva","c":"SubstringSearch","l":"SubstringSearch(String)","u":"%3Cinit%3E(java.lang.String)"}];updateSearchResults();
