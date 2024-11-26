d05a3749afcfe60a1305e85a05ccf0230d009a18
Olesia Barysheva <o.barysheva@g.nsu.ru>
2024-11-26 22:26:00 +0700

    some fixes

---
+++ Task_1_4_1/src/test/java/nsu/barysheva/MainTest.java
@@ -0,0 +1,14 @@
+package nsu.barysheva;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+
+
+}
