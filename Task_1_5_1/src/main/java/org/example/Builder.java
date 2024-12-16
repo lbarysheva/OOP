39923d53dc6d8f2ebd9d59af24cab41ec3dafc86
Olesia Barysheva <o.barysheva@g.nsu.ru>
2024-12-16 18:13:26 +0700

    fixes and tests

--- Task_1_5_1/src/main/java/org/example/Builder.java
+++ Task_1_5_1/src/main/java/org/example/Builder.java
@@ -4,9 +4,7 @@ package org.example;
  * Generic builder interface for creating document elements.
  */
 public interface Builder {
-    /**
-     * Builds and returns an element.
-     * @return the constructed element
-     */
+
+
     Element build();
 }
--- Task_1_5_1/src/main/java/org/example/Element.java
+++ Task_1_5_1/src/main/java/org/example/Element.java
@@ -4,9 +4,8 @@ package org.example;
  * Abstract base class for document elements.
  */
 public abstract class Element {
-    /**
-     * Converts the element to a string representation.
-     */
+
+
     public abstract String toString();
 
     /**
