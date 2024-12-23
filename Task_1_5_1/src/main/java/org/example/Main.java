f84c95890301966cc81633b2ea7d469f4868db9a
Olesia Barysheva <o.barysheva@g.nsu.ru>
2024-12-24 06:25:27 +0700

    fixes

--- Task_1_5_1/src/main/java/org/example/Main.java
+++ Task_1_5_1/src/main/java/org/example/Main.java
@@ -2,19 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Table.Builder tableBuilder = new Table.Builder()
-                .withAlignments(Table.ALIGN_RIGHT, Table.ALIGN_LEFT)
-                .withRowLimit(8)
-                .addRow("Index", "Random");
-        for (int i = 1; i <= 5; i++) {
-            final var value = (int) (Math.random() * 10);
-            if (value > 5) {
-                tableBuilder.addRow(i, new
-                        Text.Bold(String.valueOf(value)));
-            } else {
-                tableBuilder.addRow(i, (int) (Math.random() * 10));
-            }
-        }
-        System.out.println(tableBuilder.build());
+        CodeBlock codeBlock = new CodeBlock.Builder()
+                .setCode("System.out.println(\"Hello, world!\");")
+                .setLanguage("java")
+                .build();
+
+        System.out.println(codeBlock.toString());
     }
 }
